75affcfd23364b742f3930fb8d955433bd30ebbc
d3ciee <decefemz@gmail.com>
2025-01-07 09:45:03 +0200

    add indicatior class to progress

--- progress/progress.svelte
+++ progress/progress.svelte
@@ -1,25 +1,38 @@
 <script lang="ts">
-	import { Progress as ProgressPrimitive, type WithoutChildrenOrChild } from "bits-ui";
 	import { cn } from "$lib/ui/utils.js";
+	import {
+		Progress as ProgressPrimitive,
+		type WithoutChildrenOrChild,
+	} from "bits-ui";
 
 	let {
 		ref = $bindable(null),
 		class: className,
+		indicatorClass,
+
 		max = 100,
 		value,
 		...restProps
-	}: WithoutChildrenOrChild<ProgressPrimitive.RootProps> = $props();
+	}: WithoutChildrenOrChild<ProgressPrimitive.RootProps> & {
+		indicatorClass: string;
+	} = $props();
 </script>
 
 <ProgressPrimitive.Root
 	bind:ref
-	class={cn("bg-secondary relative h-4 w-full overflow-hidden rounded-full", className)}
+	class={cn(
+		"bg-secondary relative h-4 w-full overflow-hidden rounded-full",
+		className,
+	)}
 	{value}
 	{max}
 	{...restProps}
 >
 	<div
-		class="bg-primary h-full w-full flex-1 transition-all"
+		class={twMerge(
+			"bg-primary h-full w-full flex-1 transition-all",
+			indicatorClass,
+		)}
 		style={`transform: translateX(-${100 - (100 * (value ?? 0)) / (max ?? 1)}%)`}
 	></div>
 </ProgressPrimitive.Root>
