9d2c1da85119d143ef9d704785314e40df5c12aa
d3ciee <decefemz@gmail.com>
2025-01-07 09:45:17 +0200

    fix outline flashing on tab change

--- tabs/tabs-trigger.svelte
+++ tabs/tabs-trigger.svelte
@@ -12,7 +12,7 @@
 <TabsPrimitive.Trigger
 	bind:ref
 	class={cn(
-		'relative inline-flex items-center justify-center whitespace-nowrap rounded-sm px-2 py-1.5 text-sm font-medium transition-all before:absolute before:-bottom-0.5 before:hidden before:h-0.5 before:w-full before:bg-primary before:transition-all before:duration-200 before:ease-in-out before:content-[""] hover:text-foreground focus-visible:outline-none  disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:before:block ',
+		'relative inline-flex items-center justify-center whitespace-nowrap rounded-sm px-2 py-1.5 text-sm font-medium before:absolute before:-bottom-0.5 before:hidden before:h-0.5 before:w-full before:bg-primary before:transition-all before:duration-200 before:ease-in-out before:content-[""] hover:text-foreground focus:outline-none focus-visible:outline-none  disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:before:block ',
 		className,
 	)}
 	{...restProps}
