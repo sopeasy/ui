35eefb498d558792d7d59854a5d6b5893b7837c0
d3ciee <decefemz@gmail.com>
2025-03-07 11:15:50 +0200

    change y padding on tooltip to be smaller

--- sheet/sheet-overlay.svelte
+++ sheet/sheet-overlay.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { cn } from '$lib/ui/utils.js';
-	import { Dialog as SheetPrimitive } from 'bits-ui';
+	import { cn } from "$lib/ui/utils.js";
+	import { Dialog as SheetPrimitive } from "bits-ui";
 
 	let {
 		ref = $bindable(null),
@@ -14,8 +14,8 @@
 <SheetPrimitive.Overlay
 	bind:ref
 	class={cn(
-		'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0  fixed inset-0 z-50 bg-black/60',
-		className
+		"fixed inset-0 z-50 bg-black/60  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+		className,
 	)}
 	{...restProps}
 />
